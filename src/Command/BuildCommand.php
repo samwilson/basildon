d3ffe2e364f3f1a28cb4f8e3ca02f7a3612a2f22
Sam Wilson <sam@samwilson.id.au>
2024-09-06 10:51:19 +0800

    Better readability of elapsed build time
    Fixes #84

--- src/Command/BuildCommand.php
+++ src/Command/BuildCommand.php
@@ -20,6 +20,37 @@ use Symfony\Component\Process\Process;
 
 final class BuildCommand extends CommandBase
 {
+    /**
+     * Get a human-readable representation of the time difference between the
+     * start time and now.
+     *
+     * @param float $startTime The start time (acquired from e.g. `microtime(true)`).
+     */
+    public function getTimeElapsed(float $startTime): string
+    {
+        $total = round(microtime(true) - $startTime);
+        $parts = [
+            'hour' => 60 * 60,
+            'minute' => 60,
+            'second' => 1,
+        ];
+        $amounts = [];
+        foreach ($parts as $partName => $partSize) {
+            $amount = ($total - ( $total % $partSize ) ) / $partSize;
+            $total = $total - ( $amount * $partSize);
+            if ($amount) {
+                $amounts[] = $amount . ' ' . $partName . ($amount > 1 ? 's' : '');
+            }
+        }
+        if (!$amounts) {
+            return 'no time';
+        } elseif (count($amounts) === 1) {
+            return $amounts[0];
+        } else {
+            return join(', ', $amounts);
+        }
+    }
+
     protected function configure(): void
     {
         parent::configure();
@@ -68,8 +99,7 @@ final class BuildCommand extends CommandBase
             self::$io->write('Processing site . . . ');
             $timeStartProcessing = microtime(true);
             $db->processSite($site);
-            $seconds = max(1, round(microtime(true) - $timeStartProcessing, 0));
-            self::$io->writeln('<info>OK</info> (' . $seconds . ' ' . ($seconds > 1 ? 'seconds' : 'second') . ')');
+            self::$io->writeln('<info>OK</info> (' . $this->getTimeElapsed($timeStartProcessing) . ')');
         }
 
         // Render all pages.
@@ -147,7 +177,7 @@ final class BuildCommand extends CommandBase
         self::$io->success([
             'Site output to ' . $outDir,
             'Memory usage: ' . (memory_get_peak_usage(true) / 1024 / 1024) . ' MiB',
-            'Total time: ' . round(microtime(true) - $timeStart, 1) . ' seconds',
+            'Total time: ' . $this->getTimeElapsed($timeStart),
             'Output size: ' . substr($outputSize, 0, strpos($outputSize, "\t")),
         ]);
         return 0;
