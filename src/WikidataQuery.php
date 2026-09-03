00d9bdee382a4da7e1c1a06d361da68f43d366f3
Sam Wilson <sam@samwilson.id.au>
2026-09-04 07:46:50 +0800

    Use JSON instead of XML for Wikidata query results

--- src/WikidataQuery.php
+++ src/WikidataQuery.php
@@ -5,9 +5,9 @@ declare(strict_types=1);
 namespace App;
 
 use GuzzleHttp\Client;
-use SimpleXMLElement;
 
-use function assert;
+use function array_map;
+use function json_decode;
 use function urlencode;
 
 final class WikidataQuery
@@ -37,46 +37,13 @@ final class WikidataQuery
      */
     public function fetch(): array
     {
-        $out = [];
-        $result = $this->getXml($this->query);
-        foreach ($result->results->result as $res) {
-            $out[] = $this->getBindings($res);
-        }
-
-        return $out;
-    }
-
-    /**
-     * Get the XML result of a Sparql query.
-     *
-     * @param string $query The Sparql query to execute.
-     */
-    protected function getXml(string $query): SimpleXMLElement
-    {
-        $url = 'https://' . $this->queryService . '/bigdata/namespace/wdq/sparql?query=' . urlencode($query);
+        $query = urlencode($this->query);
+        $url = 'https://' . $this->queryService . '/bigdata/namespace/wdq/sparql?format=json&query=' . $query;
         $response = $this->client->request('GET', $url);
-
-        return new SimpleXMLElement($response->getBody()->getContents());
-    }
-
-    /**
-     * Restructure the XML that comes back from the Wikidata Query Service
-     *
-     * @param SimpleXMLElement $xml The XML for one result item.
-     *
-     * @return string[]
-     */
-    protected function getBindings(SimpleXMLElement $xml): array
-    {
+        $json = json_decode($response->getBody()->getContents() ?? '', true);
         $out = [];
-        foreach ($xml->binding as $binding) {
-            assert($binding instanceof SimpleXMLElement);
-            if (isset($binding->literal)) {
-                $out[(string) $binding['name']] = (string) $binding->literal;
-            }
-            if (isset($binding->uri)) {
-                $out[(string) $binding['name']] = (string) $binding->uri;
-            }
+        foreach ($json['results']['bindings'] ?? [] as $data) {
+            $out[] = array_map(static fn ($datum) => $datum['value'], $data);
         }
 
         return $out;
